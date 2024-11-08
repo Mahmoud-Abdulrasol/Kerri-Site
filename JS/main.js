7929f7e4b613bd2d51f0dd2c49f88d5392623140
Mahmoud-Abdulrasol <mahmoudabdoadm@gmail.com>
2024-11-08 16:31:39 +0200

    Secend-site for me
    Using Html5&Css&Bootstarp

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed('#element', {
+    strings: [ 'Mahmoud Adm', 'Developer','Front-End'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true,
+});
+// ^ =====END-TYPED=====
+// ! =====AOS==========
+AOS.init();
